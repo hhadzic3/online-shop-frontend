71a780508acac2c85f499bf61ddccb66853f2302
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-09-02 21:56:06 +0200

    Sass on the point

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -9,42 +9,21 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-
-
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(3),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-}));
+import './Signup.scss'
 
 export default function SignUp() {
-  const classes = useStyles();
 
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
+      <div className='paper'>
+        <Avatar className='avatar'>
           <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className='form' noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -146,12 +125,12 @@ export default function SignUp() {
         </Grid>
             
           </Grid>
+          <div className="submit">
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
-            className={classes.submit}
           >
             Sign Up
           </Button>
@@ -162,6 +141,7 @@ export default function SignUp() {
               </Link>
             </Grid>
           </Grid>
+            </div>
         </form>
       </div>
       
