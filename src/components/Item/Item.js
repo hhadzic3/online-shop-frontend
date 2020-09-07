cf12a1d8770c479c0babe9225afbecf8b8160f9f
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-09-07 12:32:22 +0200

    Product image styled

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,46 +1,33 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import img from '../images/nike.jpg'
 import './Item.scss'
 import LocalMallOutlinedIcon from '@material-ui/icons/LocalMallOutlined';
 import {Link} from "react-router-dom";
 
-import * as ApiService from '../../ApiService/ApiService'
 
 const ConnectedItem = (props) => {
 
     const {product} = props;
 
-    const [images,
-        setImages] = React.useState([]);
-    useEffect(() => {
-        ApiService
-            .get("/api/product_images", '/' + product.id)
-            .then(res => {
-                setImages(res);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }, []);
-
     if (!product || product.length === 0) 
         return <h2>No products, sorry</h2>;
     
-    //"https://i.imgur.com/xdbHo4E.png"
     const imageUrl= 'http://localhost:8080/api/product_images/'+ product.id;
     return (
 
         <div key={product.id} className="product-card">
-            <div className="badge">{product.label
-                    ? product.label
-                    : 'Feature'}</div>
+            {product.label !== 'classic' && product.label !== 'feature' ? (
+                <div className="badge">
+                    {product.label}
+                </div>
+                ) : null
+            }
             <Link to='/detail'>
                 <div className="product-tumb">
                     <img
                         className='ima'
                         src={imageUrl}
-                        //'http://localhost:8080/api/product_images/1'
-                        alt={images.image}/>
+                        alt='Product image'/>
                     <div className="middle">
                         <LocalMallOutlinedIcon fontSize='large'/>
                     </div>
