9106022c9ff390f2e9b8153406cdf3f8f9933060
hhadzic3 <hhadzic3@etf.unsa.ba>
2020-09-22 20:01:08 +0200

    Form for seller

--- src/components/Profile/Form.js
+++ src/components/Profile/Form.js
@@ -3,11 +3,20 @@ import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import MultipleSelect from './MultipleSelect';
 
 export default function Sell() {
+  const [values, setValues] = React.useState({
+    amount: '',
+    weight: '',
+  });
+
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
   return (
     <Container component="main" maxWidth="xs">
@@ -34,19 +43,33 @@ export default function Sell() {
                 required
                 fullWidth
                 label="Price"
+                InputProps={{
+                  startAdornment: <InputAdornment position="start">$ </InputAdornment>,
+                }}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
-                variant="outlined"
-                required
-                fullWidth
                 label="Weight"
+                id="outlined-start-adornment"
+                InputProps={{
+                  startAdornment: <InputAdornment position="start">Kg</InputAdornment>,
+                }}
+                variant="outlined"
               />
             </Grid>
-            
-            
+            <Grid item xs={12}>
+              <TextField
+                id="outlined-multiline-static"
+                label="Description"
+                multiline
+                rows={4}
+                variant="outlined"
+              />
+            </Grid>
+            <MultipleSelect />
           </Grid>
+
           <Button
             type="submit"
             fullWidth
